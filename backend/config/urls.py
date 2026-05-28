1a7c0fa97d692a92aba5841fe829ff85c939342e
seokmin <s45779823@gmail.com>
2026-05-28 12:30:36 +0900

    Add health check API

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from stocks.views import watchlist_stocks
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+from config.views import health_check
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -14,4 +15,7 @@ urlpatterns = [
     path("api/watchlist/", watchlist_stocks),
     path("api/stocks/", include("stocks.urls")),
     path("api/", include("tweets.urls")),
+
+    path("", health_check),
+    path("api/health/", health_check),
 ]
